70c2e801f343bb883344cafae1fc37c3520d4630
Nochkin <github@durdom.com>
2018-01-28 01:25:25 -0500

    Minor changes

--- src/esp_motors.c
+++ src/esp_motors.c
@@ -13,6 +13,7 @@
 #include "esp_motors.h"
 
 static int16_t esp_motors_status[] = {0, 0};
+static uint32_t *esp_motors_status_p = (uint32_t*)esp_motors_status;
 
 static uint8_t pwm_pins[] = {
     ESP_MOTOR_A_SPEED,
@@ -32,46 +33,57 @@ void esp_motors_set_single(uint8_t motor_id, uint8_t motor_speed_pin, int16_t mo
 void esp_motors_set(int16_t motor_a, int16_t motor_b) {
     esp_motors_set_single(0, ESP_MOTOR_A_SPEED, motor_a, ESP_MOTOR_A_DIR);
     esp_motors_set_single(1, ESP_MOTOR_B_SPEED, motor_b, ESP_MOTOR_B_DIR);
-    pwm_start();
 }
 
 static void esp_motors_task(void *pvParameters) {
+    /*
     printf("Motor A (Speed) GPIO: %u\n", ESP_MOTOR_A_SPEED);
     printf("Motor A (Dir) GPIO: %u\n", ESP_MOTOR_A_DIR);
     printf("Motor B (Speed) GPIO: %u\n", ESP_MOTOR_B_SPEED);
     printf("Motor B (Dir) GPIO: %u\n", ESP_MOTOR_B_DIR);
+    */
 
     pwm_init(3, pwm_pins, false);
 
     esp_motors_set(0, 0);
+    pwm_start();
 
     printf("Motors inited\n");
 
     const TickType_t xMaxBlockTime = pdMS_TO_TICKS(500);
     while (1) {
         uint32_t ulNotificationValue = ulTaskNotifyTake(pdTRUE, xMaxBlockTime);
+        //uint32_t ulNotificationValue = xTaskNotifyWait(0, 0, esp_motors_status_p, xMaxBlockTime);
 
         if (ulNotificationValue != 0) {
-            taskENTER_CRITICAL();
+            printf("Motor: L:%d R:%d\n", esp_motors_status[0], esp_motors_status[1]);
+            //pwm_stop();
+            //vTaskDelay(30);
+            //taskENTER_CRITICAL();
             //xSemaphoreTake(xMutex, portMAX_DELAY);
             esp_motors_set(esp_motors_status[0], esp_motors_status[1]);
             //xSemaphoreGive(xMutex);
-            taskEXIT_CRITICAL();
-            vTaskDelay(1);
+            //taskEXIT_CRITICAL();
+            //pwm_start();
         }
+        vTaskDelay(1);
     };
 }
 
 void esp_motors_save(int16_t motor_a, int16_t motor_b) {
     //xSemaphoreTake(xMutex, portMAX_DELAY);
-    esp_motors_status[0] = motor_a;
-    esp_motors_status[1] = motor_b;
+    //esp_motors_status[0] = motor_a;
+    //esp_motors_status[1] = motor_b;
+    *esp_motors_status_p = ((uint16_t)motor_b << 16) | (uint16_t)motor_a;
+    //uint32_t task_value = (motor_b << 16) | motor_a;
     //xSemaphoreGive(xMutex);
 
     xTaskNotifyGive(xTaskToNotify);
+    //xTaskNotify(xTaskToNotify, task_value, eSetValueWithoutOverwrite);
 }
 
 void esp_motors_init() {
     //xMutex = xSemaphoreCreateMutex();
-    xTaskCreate(&esp_motors_task, "esp_motors", 256, NULL, 8, &xTaskToNotify);
+    xTaskCreate(&esp_motors_task, "esp_motors", 256, NULL, 1, &xTaskToNotify);
 }
+
